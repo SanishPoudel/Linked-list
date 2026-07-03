a1a6e8295f67aea151b563c0477e8ff87938fef2
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-07-03 12:56:58 +1000

    added append method

--- linkedList.js
+++ linkedList.js
@@ -1,10 +1,24 @@
 class Node {
-  constructor(value, nextNode) {
+  constructor(value = null, nextNode = null) {
     this.value = value;
     this.nextNode = nextNode;
   }
 }
 
 class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
 
+  append(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = new Node(value);
+    }
+  }
 }
